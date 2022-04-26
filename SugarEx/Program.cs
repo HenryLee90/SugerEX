0e58c0a7a1e53759d404a434e8f9b10f79b0abc9
Henry Lee <hlee09151@gmail.com>
2022-04-26 10:28:59 -0500

    Init

---
+++ SugarEx/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SugarEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var answer = 5;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+            Console.WriteLine(response);
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
